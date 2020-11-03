450c57925d13c88c35ec3349adeb049c401d8685
Anna Moignard <amoignard.coder@gmail.com>
2020-11-03 14:52:34 +1100

    removed space to fix pull error

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  before_action :authenticate_user!, only: [:restricted]
+  before_action :authenticate_user! , only: [:restricted]
   # this is to ensure that only a user can see this stuff 
 
   def restricted
